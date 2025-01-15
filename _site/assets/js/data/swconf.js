6d2bd4db9766ac6d376b2955dfb89651fdf91b4a
0xfl0k1 <lucadjunior@gmail.com>
2025-01-15 20:16:43 -0300

    Article

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1724205943',resources: [
+    cacheName: 'chirpy-1736982715',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
